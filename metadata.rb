e303236a2988646b720929cd2b15f2c57fb8cf58
Charlie Sanders <sanderscharlie@gmail.com>
2013-12-08 15:55:18 -0600

    Install vim and git by default
    Also updates apt. That makes this cookbook Ubuntu specific for now

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,6 @@ description      'Installs/Configures matrix'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends 'apt'
+depends 'git'
+depends 'vim'
--- recipes/default.rb
+++ recipes/default.rb
@@ -16,3 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+include_recipe "apt"
+include_recipe "git"
+include_recipe "vim"
